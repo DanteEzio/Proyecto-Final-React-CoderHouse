e09b19f4731924d35e75b01cbb22465225d38094
DanteEzio <quantum120@hotmail.com>
2022-10-06 14:02:16 -0500

    se mejoro el diseño del detail

--- src/components/ItemDetailContainer/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,13 @@
 import "./style/ItemDetail.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const ItemDetail = ({ product }) => {
   return (
     <div className="containerDetail">
-      <div className="container p-5 text-center">
-        <div className="row justify-content-md-center g-0">
-          <div className="col col-6">
+      <div className="container pt-5 pb-5 text-center productDetailC">
+        <div className="row justify-content-center g-5">
+          <div className="col col-5 imgContainerDetail">
             <img
               src={product.img}
               className="img-fluid productIMGDetail"
@@ -13,7 +15,7 @@ const ItemDetail = ({ product }) => {
             />
           </div>
           <div className="card col-4 cardDetail">
-            <div className="card-body">
+            <div className="card-body cardBodyDetail">
               <h5 className="card-title descDetail">{product.descripcion}</h5>
               <h6 className="card-subtitle mb-2 text-muted skuDetail">
                 SKU - {product.sku}
@@ -27,6 +29,23 @@ const ItemDetail = ({ product }) => {
               <p className="card-text pt-4 stockDetail">
                 Disponibles: {product.stock}pzs.
               </p>
+              <table className="table table-borderless d-flex justify-content-center">
+                <thead>
+                  <tr className="d-flex align-items-center">
+                    <th scope="col">
+                      <button type="button" class="btn btn-danger">
+                        <FontAwesomeIcon icon={faMinus} />
+                      </button>
+                    </th>
+                    <th scope="col">1</th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-success">
+                        <FontAwesomeIcon icon={faPlus} />
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
+              </table>
               <div className="d-flex justify-content-center">
                 <button type="button" className="btn btn-success addCarDetail">
                   Añadir al Carrito
